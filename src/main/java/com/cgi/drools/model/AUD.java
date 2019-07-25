c445ab925b86e090e7260ada3d55688a96d415a7
ruby.sun <ruby.sun@cgi.com>
2019-07-25 11:14:12 -0400

    first workable cut

---
+++ src/main/java/com/cgi/drools/model/AUD.java
@@ -0,0 +1,46 @@
+package com.cgi.drools.model;
+
+public class AUD {
+    String reqPortfolioType;
+    String reqSpecialCommentCode;
+    Boolean isValid;
+    Integer errorCode;
+
+    public AUD(String reqPortfolioType, String reqSpecialCommentCode) {
+        this.reqPortfolioType = reqPortfolioType;
+        this.reqSpecialCommentCode = reqSpecialCommentCode;
+        this.isValid = true;
+    }
+
+    public String getReqPortfolioType() {
+        return reqPortfolioType;
+    }
+
+    public String getReqSpecialCommentCode() {
+        return reqSpecialCommentCode;
+    }
+
+    public Boolean isValid() {
+        return isValid;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setReqPortfolioType(String reqPortfolioType) {
+        this.reqPortfolioType = reqPortfolioType;
+    }
+
+    public void setReqSpecialCommentCode(String reqSpecialCommentCode) {
+        this.reqSpecialCommentCode = reqSpecialCommentCode;
+    }
+
+    public void setValid(Boolean valid) {
+        isValid = valid;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+}
---
+++ src/test/java/com/cgi/drools/PortfolioTypeAUDRuleTest.java
@@ -0,0 +1,65 @@
+package com.cgi.drools;
+
+import com.cgi.drools.config.DroolConfig;
+import com.cgi.drools.model.AUD;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DroolConfig.class)
+public class PortfolioTypeAUDRuleTest {
+    @Autowired
+    private KieSession kieSession;
+
+    @Test
+    public void testBlankFields(){
+        AUD bothFieldsBlank = new AUD("", "");
+        kieSession.insert(bothFieldsBlank);
+        kieSession.fireAllRules();
+        assertEquals(bothFieldsBlank.getErrorCode().intValue(), 4098);
+        assertFalse(bothFieldsBlank.isValid());
+    }
+
+    @Test
+    public void testInValidPortfolioType(){
+        AUD aud = new AUD("A", "");
+        kieSession.insert(aud);
+        kieSession.fireAllRules();
+        assertEquals(aud.getErrorCode().intValue(), 0000);
+        assertFalse(aud.isValid());
+    }
+
+    @Test
+    public void testInValidSpecialCommentCode(){
+        AUD aud = new AUD("O", "E");
+        kieSession.insert(aud);
+        kieSession.fireAllRules();
+        assertEquals(aud.getErrorCode().intValue(), 0000);
+        assertFalse(aud.isValid());
+    }
+
+    @Test
+    public void testCSSpecialCommentCode(){
+        AUD aud = new AUD("C", "CS");
+        kieSession.insert(aud);
+        kieSession.fireAllRules();
+        assertEquals(aud.getErrorCode().intValue(), 4099);
+        assertFalse(aud.isValid());
+    }
+
+    @Test
+    public void testValidRequest(){
+        AUD aud = new AUD("O", "CS");
+        kieSession.insert(aud);
+        kieSession.fireAllRules();
+        assertTrue(aud.isValid());
+    }
+}
